31e3e876519b4b0e75b2ea6732efcbc789d04774
RustyDL <jason.m.garcia04@gmail.com>
2024-01-03 21:22:33 -0500

    nothing working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import './App.css'
 import PermanentDrawerLeft from './components/Sidebar'
 import SpanningTable from './components/TransactionBar'
+import BasicGrid from './components/PrimaryDashboard'
 
 function App() {
 
 
   return (
     <>
+      <BasicGrid/>
       <PermanentDrawerLeft />
       <SpanningTable />
     </>
