6742d3351db847670d7a8ff909c122de06a41ea4
RustyDL <jason.m.garcia04@gmail.com>
2024-01-03 20:48:28 -0500

    update trans box

--- src/components/TransactionBar.jsx
+++ src/components/TransactionBar.jsx
@@ -1,4 +1,4 @@
-import { mockTransactions } from "../mockData"
+import { mockTransactions } from "./mockData"
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
