430de7cf4cd6feefdea26223175a5e8f3befcc53
RustyDL <jason.m.garcia04@gmail.com>
2024-01-04 20:03:19 -0500

    5

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import Dashboard from './components/PrimaryDashboard';
 import Transactions from './components/TransactionBar';
 import Sidebar from './components/Sidebar';
 import Header from "./components/header";
-import { mockTransactions } from './components/mockData'; // Correct the import path
 import './App.css'
+import { mockTransactions } from './components/mockData'; // Correct the import path
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
+    <Sidebar />
+    <div className="content-container">
+      <Header>Transaction List</Header>
       <Dashboard />
       <Transactions />
-      <Header />
-      <Sidebar/>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
